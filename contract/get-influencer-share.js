bb87f75553931b45fdf2df0f71c74976bd44ca27
Shubham Sharma <shubham.sharma2@antiersolutions.com>
2024-06-02 18:48:46 +0530

    Minor change to the script

--- contract/get-influencer-share.js
+++ contract/get-influencer-share.js
@@ -29,6 +29,13 @@
 
 const { ethers } = await import("npm:ethers@6.12.1");
 const abiCoder = ethers.AbiCoder.defaultAbiCoder();
-const uint256Array = [1, 2, 3];
+
+const adId = args[0];
+
+const apiResponse = await Functions.makeHttpRequest({
+  url: `http://localhost:3000/api/influencer/claim/${adId}`,
+});
+
+const { data } = apiResponse;
 const encoded = abiCoder.encode(["uint256[]"], [uint256Array]);
 return ethers.getBytes(encoded);
