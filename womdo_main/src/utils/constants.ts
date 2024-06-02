29ab8da483b1b7d55dfa152aa7ebaaff0eddeaf0
Shubham Sharma <shubham.sharma2@antiersolutions.com>
2024-06-02 18:40:08 +0530

    Initial commit: mongodb setup

--- womdo_main/src/utils/constants.ts
+++ womdo_main/src/utils/constants.ts
@@ -1,27 +1,27 @@
 export const ROUTES = {
-    HOME: "/",
-    BRAND_DASHBOARD: "/brand/dashboard",
-    BRAND_PROFILE: "/brand/profile",
-    BRAND_Ads: "/brand/myAds",
-    INFLUENCERS_LISTING: "/brand/influencers",
-    
-    INFLUENCER_PROFILE: "/influencer/profile",
-    INFLUENCER_INVITATIONS: "/influencer/invitations",
-    INFLUENCER_SUBMIT_VIDEO: "/influencer/submitVideo",
-    INFLUENCER_CLAIM: "/influencer/claim",
-}
+  HOME: "/",
+  BRAND_DASHBOARD: "/brand/dashboard",
+  BRAND_PROFILE: "/brand/profile",
+  BRAND_Ads: "/brand/myAds",
+  INFLUENCERS_LISTING: "/brand/influencers",
 
-export const API_URL = 'http://localhost:3000/api/';
+  INFLUENCER_PROFILE: "/influencer/profile",
+  INFLUENCER_INVITATIONS: "/influencer/invitations",
+  INFLUENCER_SUBMIT_VIDEO: "/influencer/submitVideo",
+  INFLUENCER_CLAIM: "/influencer/claim",
+};
+
+export const API_URL = "http://localhost:3000/api/";
 
 export const API_ROUTES = {
-    GET_INFLUENCER: 'influencer',
-    GET_INFLUENCER_INVITATIONS: 'influencer/getInvitations/',
-    GET_INFLUENCER_BY_CATEGORY: '/influencer/category/',
-    GET_BRAND: 'brand',
-    GET_BRAND_AD: 'brand/getAd/',
-    BRAND_SEND_REQUEST: 'brand/send/request/'  ,
-    AI: 'ai' 
-}
+  GET_INFLUENCER: "influencer",
+  GET_INFLUENCER_INVITATIONS: "influencer/getInvitations/",
+  GET_INFLUENCER_BY_CATEGORY: "/influencer/category/",
+  GET_BRAND: "brand",
+  GET_BRAND_AD: "brand/getAd/",
+  BRAND_SEND_REQUEST: "brand/send/request/",
+  AI: "ai",
+};
 
 export const WomdoAddress = "0x4Af5E31A8d7cb92BaE8f8DB1E168CE9ad4dD51cA";
 export const UsdtAddress = "0x16a3D0bEb95D05E9c38B21Fd4Ee3672b636A102c";
